d265bd5a42caa66ed03e133f6f4d54edc7c7799b
Levente Zuck <zucklevente@gmail.com>
2021-12-06 17:12:56 +0100

    IP webcam preview

--- resources/views/camera/show.blade.php
+++ resources/views/camera/show.blade.php
@@ -5,7 +5,8 @@
         <a class="text-gray-50" href="{{ route('camera.index') }}">Back</a>
         <h2 class="text-center pb-5 text-gray-50 font-bold text-3xl">{{ $camera->name }}</h2>
         <div class="w-full bg-gray-100 shadow rounded-lg">
-            <video id="video" src="{{ $camera->url }}/video" autoplay="autoplay"/>
+
+            <iframe src="{{ $camera->url }}/video" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
         </div>
     </div>
 @endsection
